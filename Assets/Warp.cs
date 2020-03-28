8f5b77be577e9abb819cb8f283214e82d597c5ce
iago <iagomoreda1910@gmail.com>
2020-03-28 23:59:43 +0100

    Proyecto subido 3
    Cree el terreno de las distintas casas del pueblo con sus colisiones.Además cree una forma para telenstransportarse entre los distintos terrenos.

---
+++ Assets/Warp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour
+{
+    public GameObject target;
+
+    private void Awake()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") {
+            collision.transform.position = target.transform.GetChild(0).transform.position;
+        }
+    }
+}
